16d3ed5529419500a075a7ad96d0e9104bbc636a
Richard Min <richardmin97@gmail.com>
2016-05-07 17:47:03 -0700

    Updated error pages, edge cases.

--- routes/submit.js
+++ routes/submit.js
@@ -3,8 +3,18 @@ var router = express.Router();
 
 router.post('/', function(req, res)
 {
-	res.render('submit');
+
+	res.render('submit', {title: 'Nom'});
 	console.log(req.body);
+	console.log("asdf");
+});
+
+router.get('/', function(req, res)
+{
+	res.render('error', {
+    message: "Please don't refresh this page.",
+    error: {}
+  });
 	console.log("asdf");
 });
 
